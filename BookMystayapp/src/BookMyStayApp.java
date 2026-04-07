fe6c4893c573858f3d45e542feb1745030994bfc
Hy-Shravan <shravan.per26@gmail.com>
2026-04-07 10:43:58 +0530

    UC 1 - Application Entry & Welcome Message

---
+++ BookMystayapp/src/BookMyStayApp.java
@@ -0,0 +1,11 @@
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Hotel Booking System");
+
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: v1.0");
+
+        System.out.println("System is starting...");
+    }
+}
